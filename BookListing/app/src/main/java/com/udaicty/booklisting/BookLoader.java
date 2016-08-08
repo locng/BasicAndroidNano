3c535a457f86ac74822c5f4b9cf945fbc13648ce
nkcorp <nandkcorp@gmail.com>
2016-08-08 21:29:36 +0700

    Switch to Loader

---
+++ BookListing/app/src/main/java/com/udaicty/booklisting/BookLoader.java
@@ -0,0 +1,62 @@
+package com.udaicty.booklisting;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by nloc on 8/8/2016.
+ */
+public class BookLoader extends AsyncTaskLoader<List<Book>> {
+
+    private final String LOG = this.getClass().getSimpleName();
+    /**Query URL */
+    private String mUrl;
+    public BookLoader(Context context,String url){
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    public List<Book> loadInBackground() {
+        if(mUrl == null){
+            return null;
+        }
+        //Perform the network request
+        URL url = createUrl(mUrl);
+        String jsonResponse = "";
+        try {
+            jsonResponse = Utils.makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG, "Failed to request data", e);
+            return null;
+        }
+
+        List<Book> books = Utils.extractFeatureFromJson(jsonResponse);
+        return books;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            Log.e(LOG, "Error with creating URL", exception);
+            return null;
+        }
+        return url;
+    }
+}
